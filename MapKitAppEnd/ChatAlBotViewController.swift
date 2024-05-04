931a7be9e1d44dd553531f179680701806535e97
Zeyneppsevgi <zeyneppsevgi@gmail.com>
2024-05-04 13:09:52 +0300

    chatAl added

---
+++ MapKitAppEnd/ChatAlBotViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatAlBotViewController.swift
+//  MapKitAppEnd
+//
+//  Created by Zeynep Sevgi on 4.05.2024.
+//
+
+import UIKit
+
+class ChatAlBotViewController: UIViewController {
+
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var MessageTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func GonderButton(_ sender: Any) {
+        
+    }
+    
+
+}
